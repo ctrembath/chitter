7c2b1a393c435497bf0c08189e46afbe4b104c44
ctrembath <claretrembath@ymail.com>
2014-11-28 12:27:29 +0000

    added Database cleaner gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,26 @@
 ENV['RACK_ENV'] = 'test'
 
 require './server'
+require 'database_cleaner'
 
-RSpec.configure do |config|
-    config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
+  RSpec.configure do |config|
+
+    config.run_all_when_everything_filtered = true
+    config.filter_run :focus
+
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+
   end
 
-end
+

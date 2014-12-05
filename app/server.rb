039e32338718f75f7fb65d3248db4784cf096c54
ctrembath <claretrembath@ymail.com>
2014-12-05 17:23:39 +0000

    added flash

--- app/server.rb
+++ app/server.rb
@@ -1,13 +1,17 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 require './lib/posts'
 require './lib/user'
 
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
 
+
+
 enable :sessions
 set :session_secret, 'super secret'
+use Rack::Flash
 
 get '/' do
   @posts = Post.all
@@ -33,8 +37,14 @@ post '/users' do
   @user=User.create(:email => params[:email],
               :password => params[:password],
               :password_confirmation => params[:password_confirmation])
+  if @user.save
   session[:user_id] = @user.id
   redirect to('/')
+
+  else
+    flash[:notie] = "Sorry, your passwords don't match"
+    erb :"users/new"
+  end
 end
 
 

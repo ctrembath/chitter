faf78ee03825aac48bb65fadbfe87630ce7a0a15
ctrembath <claretrembath@ymail.com>
2014-12-05 14:15:43 +0000

    tests passing, can add posts

--- app/server.rb
+++ app/server.rb
@@ -9,6 +9,16 @@ get '/' do
   erb :index
 end
 
+get '/posts/new' do
+ erb :"posts/new"
+end
+
+post '/posts' do
+  story = params["story"]
+  Post.create(:story => story)
+  redirect to('/')
+end
+
 
 
 
---
+++ spec/features/adding_post_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+  feature "User adds a new post" do
+    scenario "when browsing the homepage" do
+      expect(Post.count).to eq(0)
+      visit '/'
+      add_post('hello')
+      expect(Post.count).to eq(1)
+      post = Post.first
+      expect(post.story).to eq('hello')
+    end
+
+  def add_post(story)
+    visit '/posts/new'
+    within('#new-post') do
+      fill_in 'story', :with => story
+      click_button 'Add post'
+    end
+  end
+end
